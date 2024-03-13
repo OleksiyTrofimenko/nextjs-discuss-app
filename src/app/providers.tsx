695001d473ea02d7a9b3e950576b613cf920fca1
Trofymenko Oleksii <gtalehandro@gmail.com>
2024-03-13 20:34:24 +0200

    Discuss - 1

---
+++ src/app/providers.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  )
+};
